e281f9b92407e5acc16988bd21715231625b2ec0
Miriam <miriamteresaram@gmail.com>
2024-02-18 01:58:47 -0700

    added cards to about me

--- meow-port-app/src/AboutMe.js
+++ meow-port-app/src/AboutMe.js
@@ -25,19 +25,45 @@ const AboutMe = () => {
 			</ul>
 		</div>
 		<div>
+		{/* ROW 1 - CODE FOR IMG, NAME & SOCIALS BELOW */}
 			<div className="row" >
 				<div className="col"><img src={require('./imgs//IMG_7903.jpg')} width={200} height={200} className= "img-fluid" alt=" " /></div>
 				<div className="col">
-					<div className="row" >insert name here</div>
+					<div className="row" >
+						<h2 className="my-name" >Miriam Ramirez</h2>
+					</div>
 					<div className="row" >insert socials here?</div>
 				</div>
 			</div>
-
-			<div className="row"  >
-				<div className="col"></div>
+		{/* Row 2 - CODE FOR CARD GROUP BELOW */}
+			<div className="row" >
+				<div className="card-group">
+					<div className="card1">
+						{/*<img src="..." className="card-img" alt="cat"/>*/}
+						<div className="card-body">
+							<h3 className="card-name">Get to know me!</h3>
+							<p>-Insert info about Miriam here-</p>
+						</div>
+					</div>
+					<div className="card2">
+						{/*<img src="..." className="card-img" alt="cat"/>*/}
+						<div className="card-body">
+							<h3 className="card-name">Interests & Hobbies</h3>
+							<p>-Insert Miriam's interests and hobbies here-</p>
+						</div>
+					</div>
+					<div className="card3">
+						{/* <img src="..." className="card-img" alt="cat"/> */}
+						<div className="card-body">
+							<h3 className="card-name">TBD</h3>
+							<p>-TBD-</p>
+						</div>
+					</div>
+				</div>
 			</div>
+		{/* ROW 3 - CODE FOR CAROUSEL BELOW */}
+			<div className="row" >
 
-			<div className="row">
 			</div>
 		</div>
 	</body>
