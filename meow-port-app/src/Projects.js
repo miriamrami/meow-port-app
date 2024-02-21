08219263959cc0c04933ce4027b4b9aef9cd6382
Miriam <miriamteresaram@gmail.com>
2024-02-20 20:22:55 -0700

    added if to proj file & added border rad to abtme css

--- meow-port-app/src/Projects.js
+++ meow-port-app/src/Projects.js
@@ -3,6 +3,14 @@ import { Link } from "react-router-dom";
 import "./cssFiles//Projects.css";
 
 const Projects = () => {
+
+	const myModal = document.getElementById('myModal')
+	const myInput = document.getElementById('myInput')
+
+	myModal.addEventListener('shown.bs.modal', () => {
+		myInput.focus()
+	})
+
 	return (
 	<body>
 		<div className="ProjectPage">
@@ -22,6 +30,33 @@ const Projects = () => {
 				</li>
 			</ul>
 		</div>
+		{/**/}
+		<div className="row">{/*Might change class name later*/}
+			<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#resumeModal">
+				My Resume!
+			</button>
+
+			<div className="modal fade" id="resumeModal" tabIndex="-1" aria-labelledby="resumeModalLabel" aria-hidden="true">
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h1 className="modal-title fs-5" id="resumeModalLabel">Modal Title Here</h1>
+							<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+						</div>
+						<div className="modal-body">
+
+						</div>
+						<div className="modal-footer">
+							<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+							<button type="button" className="btn btn-primary">Download</button>
+						</div>
+
+					</div>
+
+				</div>
+			</div>
+		</div>
+
 	</body>
 	);
 };
