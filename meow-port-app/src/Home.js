7f53e49a1144c144548c675d6aed4ed0af6f0888
Miriam <miriamteresaram@gmail.com>
2024-05-05 03:52:35 -0600

    changes to home css

--- meow-port-app/src/Home.js
+++ meow-port-app/src/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./cssFiles//Home.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 const Home = () => {
 	return (
 	<body>
-		<div className = "Home">
+		<div className = "home-nav">
 			<h1> </h1>
 			<ul className="nav nav-tabs">
 				<li className="nav-item">
@@ -23,11 +23,44 @@ const Home = () => {
 				</li>
 			</ul> {/**nav nav-tabs */}
 		</div>
+		<div className="home-background">
 
-		<Container>
-
+		<Container fluid>
+			<Row className="row1">
+				<Col>
+					<Card>
+						<Card.Body>
+							Howdy! I'm Miriam and this is my portfolio.
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
 		</Container>
-
+		<Container fluid>
+			<Row className="row2">
+				<Col>
+					<Card>
+						<Card.Body>
+							<Card.Title>
+								Let's Connect!
+							</Card.Title>
+							<Card.Text>
+								I've linked my socials so that we can stay in touch 
+							</Card.Text>
+							<></>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
+		<Container fluid>
+			<Row className="row3">
+				<Col>
+					Implement footer here
+				</Col>
+			</Row>
+		</Container>
+		</div>
 	</body>
 	);
 };
