1f919d12ced7b35353ced43dc227e931c71fafda
Miriam <miriamteresaram@gmail.com>
2024-05-13 23:46:48 -0600

    removed previous nav tab and added a simple? nav bar

--- meow-port-app/src/Home.js
+++ meow-port-app/src/Home.js
@@ -1,29 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./cssFiles//Home.css";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Nav, Navbar } from "react-bootstrap";
 
 const Home = () => {
 	return (
 	<body>
-		<div className = "home-nav">
-			<h1> </h1>
-			<ul className="nav nav-tabs">
-				<li className="nav-item">
-					{/* Endpoint to route to Home component */}
-					<Link className="nav-link active" aria-current="page" to="/">Home</Link>
-				</li>
-				<li className="nav-item">
-					{/* Endpoint to route to About component */}
-					<Link className="nav-link" to="/AboutMe.js">About Me</Link>
-				</li>
-				<li className="nav-item">
-					{/* Endpoint to route to Project component */}
-					<Link className="nav-link" to="/Projects.js">Projects</Link>
-				</li>
-			</ul> {/**nav nav-tabs */}
-		</div>
 		<div className="home-background">
+
+		<Navbar>
+			<Container>
+				<Navbar.Brand href="/">:3</Navbar.Brand>
+					<Nav className="my-navbar">
+						<Link className="nav-link" to="/">Home</Link>
+						<Link className="nav-link" to="/AboutMe.js">About Me</Link>
+						<Link className="nav-link" to="/Projects.js">Projects</Link>
+				</Nav>
+			</Container>
+		</Navbar>
+
+
 
 		<Container fluid>
 			<Row className="row1">
