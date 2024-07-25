198c482a061e80b2dd8ab297f528ef04d8bc29c8
Miriam <miriamteresaram@gmail.com>
2024-07-25 05:21:39 -0400

    added more images to the demo folder and moved around the rows/cols from the project.js file

--- meow-port-app/src/Projects.js
+++ meow-port-app/src/Projects.js
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
 import "./cssFiles//Projects.css";
-import { Navbar, Nav, Container, Row, Col, Accordion } from "react-bootstrap";
+import { Navbar, Nav, Container, Row, Col, Accordion, Image} from "react-bootstrap";
+import WA1 from "./Demos/wApp1.png";
+import WA2 from "./Demos/wApp2.png";
+import WA3 from "./Demos/wApp3.png";
+import WA4 from "./Demos/wApp4.png";
+
 
 const Projects = () => {
 
@@ -23,7 +28,41 @@ const Projects = () => {
       			<Accordion.Item eventKey="0">
         			<Accordion.Header>Accordion Item #1</Accordion.Header>
         				<Accordion.Body>
-							Enter description of info of project here
+							<Container>
+								<Row>
+									<Col>app name </Col>
+									<Col>brief description</Col>
+								</Row>
+								<Row>
+									<Col>videos?</Col>
+									<Col>
+										<Row>
+											<Col></Col>
+											<Col></Col>
+											<Col></Col>
+											<Col></Col>
+										</Row>
+										<Row>
+											<Col>
+												<Image src={WA1} rounded fluid/>
+											</Col>
+											<Col>
+												<Image src={WA2} rounded fluid/>
+											</Col>
+											<Col><Image src={WA3} rounded fluid/></Col>
+											<Col><Image src={WA4} rounded fluid/></Col>
+										</Row>
+										<Row>
+											<Col></Col>
+											<Col></Col>
+											<Col></Col>
+											<Col></Col>
+										</Row>
+									</Col>
+								</Row>
+							</Container>
+
+
         				</Accordion.Body>
       			</Accordion.Item>
       			<Accordion.Item eventKey="1">
